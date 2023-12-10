818193e879a2fca33bbddd75c9b5889424c673ef
CarlosMoreno3 <101369176+CarlosMoreno3@users.noreply.github.com>
2023-12-10 00:22:32 -0600

    Creacion tickets completado

--- src/main/java/com/Proyecto/Controller/MenusController.java
+++ src/main/java/com/Proyecto/Controller/MenusController.java
@@ -3,10 +3,13 @@ package com.Proyecto.controller;
 import com.Proyecto.Service.ComentariosService;
 import com.Proyecto.Service.TicketService;
 import com.Proyecto.ServiceImp.FirebaseStorageServiceImpl;
+import com.Proyecto.domain.Ticket;
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,6 +39,14 @@ public class MenusController {
         //List<Perfil> listadoPerfiles = perfilService.getPerfiles(true);
         //model.addAttribute("perfiles", listadoPerfiles);
         return "menus/crearTiquete";
+    }
+    
+    @PostMapping("/guardarTicket")
+    public String ticketGuardar(Ticket ticket) {    
+        ticket.setEstado("activo");
+        ticket.setFechaCreacion(LocalDateTime.now().toString());
+        ticketService.save(ticket);
+        return "redirect:/menus/tickets";
     }
 
     @GetMapping("/notificaciones")
