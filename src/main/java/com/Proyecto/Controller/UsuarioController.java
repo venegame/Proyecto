b100df66f6d0e9cca37640735034ef8bf9ca86b4
venegame <melany.venegas2@gmail.com>
2023-12-11 23:07:08 -0600

    Small fixes para usuarios

--- src/main/java/com/Proyecto/Controller/UsuarioController.java
+++ src/main/java/com/Proyecto/Controller/UsuarioController.java
@@ -4,10 +4,10 @@
  */
 package com.Proyecto.Controller;
 
+import com.Proyecto.Service.FirebaseStorageService;
 import com.Proyecto.Service.UsuarioService;
 import com.Proyecto.domain.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +15,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-    
+
     @Autowired
     private UsuarioService usuarioService;
     
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+
     @GetMapping("/listado")
     public String listado(Model model) {
         var usuarios = usuarioService.getUsuarios();
@@ -30,42 +34,38 @@ public class UsuarioController {
         model.addAttribute("totalUsuarios", usuarios.size());
         return "/usuario/listado";
     }
-    
-    @GetMapping("/modificar/{idUsuario}")
-    public String usuarioModificar(Usuario usuario, Model model) {
-        usuario = usuarioService.getUsuario(usuario);
-        model.addAttribute("usuario", usuario);
-        return "/usuario/modifica";          
+
+    @GetMapping("/nuevo")
+    public String usuarioNuevo(Usuario usuario) {
+        return "/usuario/modifica";
     }
-    
+
     @PostMapping("/guardar")
     public String usuarioGuardar(Usuario usuario,
             @RequestParam("imagenFile") MultipartFile imagenFile) {
-        
-        boolean nuevo = true;
-        //Validar si es una creacion o modificacion (Si trae ID)
-        if (usuario.getIdUsuario() != 0) {
-            nuevo =  false;
-            Usuario actual = usuarioService.getUsuario(usuario);
-            usuario.setPassword(actual.getPassword());
-            usuario.setUsername(actual.getUsername());
-            usuario.setRoles(actual.getRoles());
-            if (imagenFile.isEmpty()) {
-                usuario.setRutaImagen(actual.getRutaImagen());
-            }
+		usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
+        if (!imagenFile.isEmpty()) {
+            usuarioService.save(usuario,false);
+            usuario.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile,
+                            "usuario",
+                            usuario.getIdUsuario()));
         }
-        else {
-            usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
-            usuario.setActivo(true); // Para crearlo siempre activo
-        }
-        usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
-        usuarioService.save(usuario, nuevo);
+        usuarioService.save(usuario,true);
         return "redirect:/usuario/listado";
     }
-    
+
     @GetMapping("/eliminar/{idUsuario}")
     public String usuarioEliminar(Usuario usuario) {
         usuarioService.delete(usuario);
         return "redirect:/usuario/listado";
+    }
+
+    @GetMapping("/modificar/{idUsuario}")
+    public String usuarioModificar(Usuario usuario, Model model) {
+        usuario = usuarioService.getUsuario(usuario);
+        model.addAttribute("usuario", usuario);
+        return "/usuario/modifica";
     }
 }
